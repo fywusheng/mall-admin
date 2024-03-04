27243849858dd9569fd779c7e188525f1fa57d07
SHI <851051279@qq.com>
2024-03-04 16:56:16 +0800

    会员订单列表接口对接完成

--- src/views/trade-module/oms/order-list.vue
+++ src/views/trade-module/oms/order-list.vue
@@ -1,108 +1,183 @@
 <template>
   <div class="main-content">
     <el-tabs v-model="activeType" type="card" @tab-click="handleClick" style="padding-top:0">
-      <el-tab-pane label="交易订单" name="1" :disabled="disabled"></el-tab-pane>
-      <el-tab-pane label="会员订单" name="2" :disabled="disabled"></el-tab-pane>
-    </el-tabs>
-    <el-row class="mb-2">
-      <el-form :inline="true">
-        <el-form-item label="">
-          <el-input v-model="searchParams.orderId" placeholder="请输入订单编号..." clearable
-            size="mini"></el-input>
-        </el-form-item>
-        <el-form-item label="">
-          <el-input v-model="searchParams.userLoginName" placeholder="请输入会员账号..." clearable
-            size="mini"></el-input>
-        </el-form-item>
-        <!-- 交易订单订单状态 -->
-        <el-form-item label="" v-if="activeType == 1">
-          <el-select v-model="searchParams.orderStatus" size="mini" clearable collapse-tags
-            filterable style="width:100%" placeholder="请选择订单状态...">
-            <el-option v-for="item in orderStatusOptions" :key="item.code" :label="item.name"
-              :value="item.code"></el-option>
-          </el-select>
-        </el-form-item>
-        <!-- 会员订单订单状态 -->
-        <el-form-item label="" v-if="activeType == 2">
-          <el-select v-model="searchParams.orderStatus" size="mini" clearable collapse-tags
-            filterable style="width:100%" placeholder="请选择订单状态...">
-            <el-option v-for="item in vipOrderStatusOptions" :key="item.code" :label="item.name"
-              :value="item.code"></el-option>
-          </el-select>
-        </el-form-item>
-        <!-- TODO 新增订单类型 -->
-        <el-form-item label="" v-if="activeType == 1">
-          <el-select v-model="searchParams.orderStatus" size="mini" clearable collapse-tags
-            filterable style="width:100%" placeholder="请选择订单类型...">
-            <el-option v-for="item in orderTypeOptions" :key="item.code" :label="item.name"
-              :value="item.code"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" icon="el-icon-search" @click="loadData"
-            size="mini">查询</el-button>
-        </el-form-item>
-      </el-form>
-    </el-row>
-    <el-table class="custom-table" :data="dataList"
-      :header-cell-style="{background:'#FAF9F7',color:'#000000'}" size="mini" v-loading="loading">
-      <div slot="empty" class="empty-wrap">
-        <i class="iconfont icon-tishi"></i><span>系统暂无数据</span>
-      </div>
-      <el-table-column type="index" label="序号" align="center" width="50px"></el-table-column>
-      <el-table-column prop="orderId" label="订单编号"  align="center"></el-table-column>
-      <!-- TODO 新增订单类型字段 -->
-      <el-table-column v-if="activeType == 1" prop="orderType" label="订单类型" width="160px" align="center"></el-table-column>
+      <!-- 交易订单 -->
+      <el-tab-pane label="交易订单" name="1">
+        <el-row class="mb-2">
+          <el-form :inline="true">
+            <el-form-item label="">
+              <el-input v-model="searchParams.orderId" placeholder="请输入订单编号..." clearable
+                size="mini"></el-input>
+            </el-form-item>
+            <el-form-item label="">
+              <el-input v-model="searchParams.userLoginName" placeholder="请输入会员账号..." clearable
+                size="mini"></el-input>
+            </el-form-item>
+            <!-- 交易订单订单状态 -->
+            <el-form-item label="">
+              <el-select v-model="searchParams.orderStatus" size="mini" clearable collapse-tags
+                filterable style="width:100%" placeholder="请选择订单状态...">
+                <el-option v-for="item in orderStatusOptions" :key="item.code" :label="item.name"
+                  :value="item.code"></el-option>
+              </el-select>
+            </el-form-item>
+            <!-- TODO 新增订单类型 -->
+            <el-form-item label="">
+              <el-select v-model="searchParams.orderStatus" size="mini" clearable collapse-tags
+                filterable style="width:100%" placeholder="请选择订单类型...">
+                <el-option v-for="item in orderTypeOptions" :key="item.code" :label="item.name"
+                  :value="item.code"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" icon="el-icon-search" @click="loadData"
+                size="mini">查询</el-button>
+            </el-form-item>
+          </el-form>
+        </el-row>
+        <el-table class="custom-table" :data="dataList"
+          :header-cell-style="{background:'#FAF9F7',color:'#000000'}" size="mini" v-loading="loading">
+          <div slot="empty" class="empty-wrap">
+            <i class="iconfont icon-tishi"></i><span>系统暂无数据</span>
+          </div>
+          <el-table-column type="index" label="序号" align="center" width="50px"></el-table-column>
+          <el-table-column prop="orderId" label="订单编号"  align="center"></el-table-column>
+          <!-- TODO 新增订单类型字段 -->
+          <el-table-column v-if="activeType == 1" prop="orderType" label="订单类型" width="160px" align="center"></el-table-column>
 
-      <el-table-column prop="orderAmount" label="订单金额" width="70px"
-        align="center"></el-table-column>
-      <el-table-column prop="payableAmount" label="应付金额" width="70px"
-        align="center"></el-table-column>
-      <el-table-column prop="paidAmount" label="实付金额" width="70px" align="center"></el-table-column>
-      <el-table-column prop="orderStatusLabel" label="订单状态" align="center"
-        width="80px"></el-table-column>
-      <el-table-column prop="paymentMethod" label="支付方式" align="center" width="80px"
-        :formatter="formatPaymentMethod"></el-table-column>
-      <el-table-column prop="userName" label="下单人" align="center" width="100px"
-        show-overflow-tooltip></el-table-column>
-      <el-table-column prop="userLoginName" label="下单账号" align="center"
-        width="120px"></el-table-column>
-      <!-- 收货地址字段，会员订单不显示 -->
-      <el-table-column v-if="activeType == 1" prop="userName" label="收货地址" :formatter="formatReceiveAddress"
-        show-overflow-tooltip></el-table-column>
-      <el-table-column prop="createdTime" label="下单时间" align="center"
-        width="150px"></el-table-column>
-      <el-table-column prop="" label="操作" align="center" width="120px">
-        <template slot-scope="scope">
-          <el-button type="default" icon="el-icon-document" size="mini"
-            @click="forward2DetailsPage(scope.row)">详情</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <el-pagination background v-show="totalCount > 10" @size-change="changeSize"
-      @current-change="changePage" :page-size="pageSize"
-      layout="total, slot, jumper, prev, pager, next" :total="totalCount">
-    </el-pagination>
+          <el-table-column prop="orderAmount" label="订单金额" width="70px"
+            align="center"></el-table-column>
+          <el-table-column prop="payableAmount" label="应付金额" width="70px"
+            align="center"></el-table-column>
+          <el-table-column prop="paidAmount" label="实付金额" width="70px" align="center"></el-table-column>
+          <el-table-column prop="orderStatusLabel" label="订单状态" align="center"
+            width="80px"></el-table-column>
+          <el-table-column prop="paymentMethod" label="支付方式" align="center" width="80px"
+            :formatter="formatPaymentMethod"></el-table-column>
+          <el-table-column prop="userName" label="下单人" align="center" width="100px"
+            show-overflow-tooltip></el-table-column>
+          <el-table-column prop="userLoginName" label="下单账号" align="center"
+            width="120px"></el-table-column>
+          <!-- 收货地址字段，会员订单不显示 -->
+          <el-table-column v-if="activeType == 1" prop="userName" label="收货地址" :formatter="formatReceiveAddress"
+            show-overflow-tooltip></el-table-column>
+          <el-table-column prop="createdTime" label="下单时间" align="center"
+            width="150px"></el-table-column>
+          <el-table-column prop="" label="操作" align="center" width="120px">
+            <template slot-scope="scope">
+              <el-button type="default" icon="el-icon-document" size="mini"
+                @click="forward2DetailsPage(scope.row)">详情</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-pagination background v-show="totalCount > 10" @size-change="changeSize"
+          @current-change="changePage" :page-size="pageSize"
+          layout="total, slot, jumper, prev, pager, next" :total="totalCount">
+        </el-pagination>
+      </el-tab-pane>
+
+      
+      <!-- 会员订单 -->
+      <el-tab-pane label="会员订单" name="2">
+        <el-row class="mb-2">
+          <el-form :inline="true">
+            <el-form-item label="">
+              <el-input v-model="vipSearchParams.orderId" placeholder="请输入订单编号..." clearable
+                size="mini"></el-input>
+            </el-form-item>
+            <el-form-item label="">
+              <el-input v-model="vipSearchParams.userPhone" placeholder="请输入会员账号..." clearable
+                size="mini"></el-input>
+            </el-form-item>
+            <el-form-item label="">
+              <el-select v-model="vipSearchParams.orderStatus" size="mini" clearable collapse-tags
+                filterable style="width:100%" placeholder="请选择订单状态...">
+                <el-option v-for="item in vipOrderStatusOptions" :key="item.code" :label="item.name"
+                  :value="item.code"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" icon="el-icon-search" @click="loadVipData"
+                size="mini">查询</el-button>
+            </el-form-item>
+          </el-form>
+        </el-row>
+        <el-table class="custom-table" :data="vipDataList"
+          :header-cell-style="{background:'#FAF9F7',color:'#000000'}" size="mini" v-loading="loading">
+          <div slot="empty" class="empty-wrap">
+            <i class="iconfont icon-tishi"></i><span>系统暂无数据</span>
+          </div>
+          <el-table-column type="index" label="序号" align="center" width="50px"></el-table-column>
+          <el-table-column prop="orderId" label="订单编号"  align="center"></el-table-column>
+          <el-table-column prop="orderAmount" label="订单金额" width="70px"
+            align="center"></el-table-column>
+          <el-table-column prop="payAmount" label="应付金额" width="70px"
+            align="center"></el-table-column>
+          <el-table-column prop="paymentAmount" label="实付金额" width="70px" align="center"></el-table-column>
+          <el-table-column prop="orderStatus" label="订单状态" align="center"
+            width="80px" :formatter="formatOrderStatusLabel"></el-table-column>
+          <el-table-column prop="paymentMethod" label="支付方式" align="center" width="80px"
+            :formatter="formatPaymentMethod"></el-table-column>
+          <el-table-column prop="crterName" label="下单人" align="center" width="100px"
+            show-overflow-tooltip></el-table-column>
+          <el-table-column prop="updterName" label="下单账号" align="center"
+            width="120px"></el-table-column>
+          <el-table-column prop="orderTime" label="下单时间" align="center"
+            width="150px"></el-table-column>
+          <el-table-column prop="" label="操作" align="center" width="120px">
+            <template slot-scope="scope">
+              <el-button type="default" icon="el-icon-document" size="mini"
+                @click="forward2DetailsPage(scope.row)">详情</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-pagination background v-show="vipTotalCount > 10" @size-change="changeVipSize"
+          @current-change="changeVipPage" :page-size="vipPageSize"
+          layout="total, slot, jumper, prev, pager, next" :total="vipTotalCount">
+        </el-pagination>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 <script>
 import { fetch, post } from '@/utils/http-client'
-
+import { post as nepspPost } from '@/utils/http-nepsp'
 export default {
   name: '',
   data() {
     return {
       pageNo: 1,
       pageSize: 15,
+      vipPageNo: 1,
+      vipPageSize: 15,
       dataList: [],
-      totalCount: 20,
+      vipDataList: [],
+      totalCount: 0,
+      vipTotalCount: 0,
       loading: false,
+      vipLoading: false,
       // 交易订单状态
       orderStatusOptions: [{ name: '待支付', code: '10' }, { name: '待发货', code: '20' }, { name: '待收货', code: '30' }, { name: '待评价', code: '50' }, { name: '交易完成', code: '40' }, { name: '已取消', code: '90' }],
       // 会员订单状态 TODO 
-      vipOrderStatusOptions: [{ name: '已完成', code: 'xxx' }, { name: '待支付', code: '10' }, { name: '已关闭', code: 'xxx' }, { name: '已退款', code: 'xxx' }],
+      vipOrderStatusOptions: [{ name: '已完成', code: '3' }, { name: '待支付', code: '1' }, { name: '已关闭', code: '4' }, { name: '已退款', code: '6' }],
+      // 列表中的字典用到
+      vipOrderStatusOptionsCopy: [
+        { name: '未支付', code: '1' },
+        { name: '已支付', code: '2' },
+        { name: '已完成', code: '3' },
+        { name: '关闭订单', code: '4' },
+        { name: '部分退款', code: '5' },
+        { name: '已退款', code: '6' },
+        { name: '退款中', code: '7' },
+      ],
       orderTypeOptions: [{ name: '商品购买', code: '1' }, { name: '积分兑换', code: '2' }],
       searchParams: {},
+      vipSearchParams: {
+        orderSource: 6,
+        orderId: '',
+        userPhone: '',
+        orderStatus: '',
+      },
       dialogList: [],
       showDialog: false,
       activeType: '1',
@@ -111,6 +186,7 @@ export default {
   },
   async mounted() {
     this.loadData()
+    this.loadVipData()
   },
   methods: {
     handleClick(tab, event) {
@@ -118,13 +194,17 @@ export default {
       this.pageNo = 1
       this.searchParams = {}
       // this.searchParams.queryType = this.activeType
-      this.loadData();
+      // this.loadData();
     },
     formatPaymentMethod: function (row, column) {
       return row.paymentMethod === 'alipayMobile' ? '支付宝' : row.paymentMethod === 'wechatMobile' ? '微信支付' : row.paymentMethod === 'cash_pay' ? '余额支付' : '--'
     },
     formatReceiveAddress: function (row, column) {
       return row.receiveCityName + row.receiveAreaName + row.receiveAddress
+    },
+    formatOrderStatusLabel (row, column) {
+      const res = this.vipOrderStatusOptionsCopy.find(item => item.code == row.orderStatus)
+      return res ? res.name : '--'
     },
     changePage(pageNo) {
       this.pageNo = pageNo
@@ -153,6 +233,35 @@ export default {
         this.$message.error(result.msg)
       }
       this.loading = false
+    },
+
+    changeVipPage(pageNo) {
+      this.vipPageNo = pageNo
+      this.loadVipData()
+    },
+    changeVipSize(pageSize) {
+      this.pageSize = pageSize
+      this.vipPageNo = 1
+      this.loadVipData()
+    },
+    async loadVipData() {
+      this.vipLoading = true
+      const params = {
+        pageNum: this.vipPageNo,
+        pageSize: this.pageSize,
+        ...this.vipSearchParams
+      }
+      const result = await nepspPost('/offlineshopping/order/findOrderPage', {data: params})
+      if (result.code == 0) {
+        this.$nextTick(() => {
+          this.vipDataList = result.data.list
+          this.vipTotalCount = result.data.total * 1
+          //this.pageSize = result.data.limit;
+        })
+      } else {
+        this.$message.error(result.msg)
+      }
+      this.vipLoading = false
     },
     forward2DetailsPage(row) {
       this.$router.push({ name: 'Order-Detail', params: { orderCode: row.orderId, orderType: this.activeType } })
