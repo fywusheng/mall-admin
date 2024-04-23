1b6dc5ae9cb4a063073025fd0d5e265b84e0b4ad
SHI <851051279@qq.com>
2024-04-23 14:43:23 +0800

    删除富文本编辑器的 菜单项

--- src/components/Tinymce/index.vue
+++ src/components/Tinymce/index.vue
@@ -146,6 +146,7 @@ export default {
         object_resizing: false,
         toolbar: this.toolbar.length > 0 ? this.toolbar : toolbar,
         menubar: this.menubar,
+        removed_menuitems: 'image, media, codesample, template, anchor',
         plugins: plugins,
         end_container_on_empty_block: true,
         powerpaste_word_import: 'clean',
